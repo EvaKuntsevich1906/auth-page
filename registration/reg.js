96c589708a81aa320a1a2adb79a7841397ca6101
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-21 23:38:34 +0300

    regex

--- registration/reg.js
+++ registration/reg.js
@@ -16,13 +16,17 @@ function validationPassword(validationEmail) {
         try {
             const passwordinput = document.querySelector(".passwordinput").value;
             const dblpasswordinput = document.querySelector(".dblpasswordinput").value;
-            if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm.test(passwordinput)) throw new Error(""))
+            if (passwordinput.length < 8) throw new Error("Пароль слишком короткий. Введите пароль, длиной не менее, чем 8 символов.")
+            if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm.test(passwordinput))
+                throw new Error("Пароль должен содержать минимум 8 символов, 1 заглавную букву и минимум 1 спецсимвол. Введите пароль снова")
+            if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm.test(dblpasswordinput))
+                throw new Error("Пароль должен содержать минимум 8 символов, 1 заглавную букву и минимум 1 спецсимвол. Введите пароль снова") 
+            if (!passwordinput === dblpasswordinput) throw new Error("Пароли не совпадают. Введите пароли еще раз")
         } catch (err) {
-
+            alert(err.message)
         }
     }
 }
 
 
-
-startbutton.addEventListener("click", validationData)
+startbutton.addEventListener("click", validationPassword)
